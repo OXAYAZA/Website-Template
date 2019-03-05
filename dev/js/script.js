b5bc4bdd908a579cf086e7bb17cbf58ffb2c3f35
OXAYAZA <gashishmarihuanovich@gmail.com>
2019-03-06 01:24:27 +0200

    ADD: eslint

--- dev/js/script.js
+++ dev/js/script.js
@@ -1,4 +1,12 @@
 'use strict';
+
+/*
+global
+	WOW
+	Isotope
+	Swiper
+*/
+
 // Global variables
 var plugins = {
 
--- gulpfile.js
+++ gulpfile.js
@@ -54,25 +54,48 @@ if ( global.config.buildRules ) util.genBuildTasks();
 
 // Code Lint
 const
-	stylelint = require( 'gulp-stylelint' ),
-	puglint = require( 'gulp-pug-linter' ),
+	stylelint  = require( 'gulp-stylelint' ),
+	puglint    = require( 'gulp-pug-linter' ),
+	eslint     = require( 'gulp-eslint' ),
 	lintConfig = require( './package.json' );
 
 function styleLintFormatter ( report ) {
-	report.forEach( function( item ) {
-		item.warnings.forEach( function( warning ) {
-			console.log( gutil.colors.red( '×' ), warning.text );
-			console.log( `${item.source}:${warning.line}:${warning.column}\n` );
+	report.forEach( function( file ) {
+		file.warnings.forEach( function( warning ) {
+			console.log( gutil.colors.red( '⨂' ), warning.text.replace( /\s*\(.*\)/, '' ), gutil.colors.gray( `[${warning.rule}]` ) );
+			console.log( `${file.source}:${warning.line}:${warning.column}\n` );
 		});
 	});
 }
 
 function pugLintForamtter ( errors ) {
-	errors.forEach( function ( error, index ) {
+	errors.forEach( function ( error ) {
 		let tmp = error.toJSON();
-		console.log( gutil.colors.red( '×' ), tmp.msg );
+		console.log( gutil.colors.red( '⨂' ), tmp.msg, gutil.colors.gray( `[${tmp.code}]` ) );
 		console.log( `${tmp.filename}:${tmp.line}:${tmp.column}\n` );
 	});
+}
+
+function esLintForamtter ( report ) {
+	report.forEach( function( file ) {
+		file.messages.forEach( function( message ) {
+			switch( message.severity ) {
+				case 0:
+					console.log( gutil.colors.green( '☑' ), message.message, gutil.colors.gray( `[${message.ruleId}]` ) );
+					break;
+				case 1:
+					console.log( gutil.colors.yellow( '⚠' ), message.message, gutil.colors.gray( `[${message.ruleId}]` ) );
+					break;
+				case 2:
+					console.log( gutil.colors.red( '⨂' ), message.message, gutil.colors.gray( `[${message.ruleId}]` ) );
+					break;
+			}
+			console.log( `${file.filePath}:${message.line}:${message.column}\n` );
+		});
+	});
+
+	console.log( 'errors:', gutil.colors.red( report.errorCount ) );
+	console.log( 'warnings:', gutil.colors.yellow( report.warningCount ) );
 }
 
 function lintCss () {
@@ -98,8 +121,15 @@ function lintPug () {
 		.pipe( puglint({ reporter: pugLintForamtter }));
 }
 
+function lintJs () {
+	return gulp.src( 'dev/js/**/!(*.min).js' )
+		.pipe( eslint( lintConfig.jsLintConfig ) )
+		.pipe( eslint.format( esLintForamtter ) );
+}
+
 lintCss.displayName = 'Lint CSS';
 lintScss.displayName = 'Lint SCSS';
 lintPug.displayName = 'Lint PUG';
+lintJs.displayName = 'Lint JS';
 
-gulp.task( 'Lint Code', gulp.series( lintScss, lintCss, lintPug ) );
+gulp.task( 'Lint Code', gulp.series( lintJs, lintScss, lintCss, lintPug ) );
